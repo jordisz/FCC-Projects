52c7c06c8b1142ea8fb3aa12f8e1c9a6507a7c74
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2020-11-20 11:19:52 +0100

    Backup of server.js

---
+++ 14-File Metadata Microservice/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res){
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size})
+})
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
